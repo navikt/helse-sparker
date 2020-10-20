cc71b851d0d15315c7028215107ee7756e577501
Aleksander Andresen <caeses@gmail.com>
2020-10-20 10:34:54 +0200

    Bump deps
    Co-authored-by: Jakob Havstein Eriksen  <jakob.havstein.eriksen@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,10 +22,15 @@ dependencies {
     implementation("org.flywaydb:flyway-core:7.0.4")
     implementation("com.zaxxer:HikariCP:3.4.5")
     implementation("no.nav:vault-jdbc:1.3.7")
+    implementation("org.postgresql:postgresql:42.2.13")
     implementation("com.github.seratch:kotliquery:1.3.1")
     implementation("org.slf4j:slf4j-api:1.3.1")
 
     testImplementation("no.nav:kafka-embedded-env:2.5.0")
+    constraints {
+        testImplementation("org.apache.kafka:kafka_2.12:2.6.0")
+        testImplementation("org.glassfish.jersey.media:jersey-media-jaxb:2.3.1")
+    }
     testImplementation("com.opentable.components:otj-pg-embedded:0.13.3")
     testImplementation("io.mockk:mockk:1.10.2")
 
