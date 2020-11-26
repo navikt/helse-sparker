e06a3b81dce4b4b05954222418027a8904c4c94b
Aleksander Andresen <caeses@gmail.com>
2020-11-26 16:09:32 +0100

    Fiks test
    Co-authored-by: Jakob Havstein Eriksen  <jakob.havstein.eriksen@nav.no>
    Co-authored-by: David Steinsland <david@steinsland.email>
    Co-authored-by: Christian Bulow Skovborg <christian.bulow.skovborg@nav.no>
    Co-authored-by: Morten Tholander <morten.tholander@nav.no>

--- src/test/kotlin/no/nav/helse/sparker/ComponentTest.kt
+++ src/test/kotlin/no/nav/helse/sparker/ComponentTest.kt
@@ -68,7 +68,7 @@ internal class ComponentTest {
         val consumer = KafkaConsumer<String, String>(baseConfig().toConsumerConfig())
         consumer.assign(listOf(TopicPartition(topic, 0)))
         consumer.seekToBeginning(consumer.assignment())
-        assertEquals(62, consumer.poll(Duration.ofMillis(100)).count()) //TODO
+        assertEquals(63, consumer.poll(Duration.ofMillis(100)).count()) //TODO
     }
 
     @AfterAll
